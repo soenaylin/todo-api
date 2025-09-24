f1e7450101a76ff048104d73fe50096c039254aa
Soe Nay Lin <soenaylin.g@gmail.com>
2025-09-24 10:17:51 +0630

    todo api

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const app = express();
+
+const { MongoClient, ObjectId } = require("mongodb");
+const client = new MongoClient("mongodb://localhost");
+const db = client.db("todo");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const cors = require("cors");
+app.use(cors());
+
+app.get("/tasks", async (req, res) => {
+  const data = await db.collection("tasks").aggregate([
+    {
+        $sort: {
+            created: -1,
+        },
+    },
+  ]).toArray();
+
+  //   setTimeout(() => {
+  //     return res.json(data);
+  //   }, 2000)
+
+  return res.json(data);
+});
+
+app.get("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const data = await db.collection("tasks").findOne({ _id: new ObjectId(id) });
+
+  return res.json(data);
+});
+
+app.post("/tasks", async (req, res) => {
+  const { name } = req.body;
+  if (!name) return res.status(400).json({ msg: "name require" });
+
+  const result = await db.collection("tasks").insertOne({
+    name,
+    done: false,
+    created: new Date(),
+  });
+
+  return res.json({ _id: result.insertedId, name, done: false });
+});
+
+app.put("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!name) return res.status(400).json({ msg: "name require" });
+
+  try {
+    const result = await db
+      .collection("tasks")
+      .updateOne({ _id: new ObjectId(id) }, { $set: { name } });
+
+    return res.json(result);
+  } catch (e) {
+    return res.sendStatus(500);
+  }
+});
+
+app.put("/tasks/:id/toggle", async (req, res) => {
+  const { id } = req.params;
+
+  const data = await db.collection("tasks").findOne({ _id: new ObjectId(id) });
+
+  const result = await db
+    .collection("tasks")
+    .updateOne({ _id: new ObjectId(id) }, { $set: { done: !data.done } });
+
+  return res.json(result);
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const result = await db
+    .collection("tasks")
+    .deleteOne({ _id: new ObjectId(id) });
+
+  return res.json(result);
+});
+
+app.listen(8888, () => {
+  console.log("Todo API running at 8888");
+});
