978c5b5846fe74ef1b2b6c78a1a2b0726a6acbc4
Soe Nay Lin <soenaylin.g@gmail.com>
2025-11-27 11:00:26 +0630

    fix mongodb connection

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => console.log("MongoDB connected"))
+	.catch((err) => console.error(err));
+
 const express = require("express");
 const app = express();
 
