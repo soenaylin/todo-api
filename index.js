b065528c7820ec6e76629c276b5de83eb38f21aa
Soe Nay Lin <soenaylin.g@gmail.com>
2025-11-27 21:22:22 +0630

    replace dotenv import syntax

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 mongoose
